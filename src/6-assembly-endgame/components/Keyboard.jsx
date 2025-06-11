065b78ab14e2cb4f1cb6985d04cf410152d5a78e
pchristou <pierischristou@gmail.com>
2025-06-11 16:29:19 +0100

    6 - assembly: endgame (hangman) - simplified

--- src/6-assembly-endgame/components/Keyboard.jsx
+++ src/6-assembly-endgame/components/Keyboard.jsx
@@ -5,11 +5,11 @@ export default function Keyboard({ currentWord, guess, guessedLetters }) {
     const alphabet = [...Array(26).keys()].map((n) => String.fromCharCode(65 + n).toLowerCase());
     const buttons = alphabet.map((char) => {
 
-        const defaultState = !guessedLetters.includes(char);
-        const correctGuess = guessedLetters.some(letter => currentWord.includes(letter) && char === letter);
-        const incorrectGuess = !guessedLetters.some(letter => currentWord.includes(letter) && char === letter);
+        const isGuessed = guessedLetters.includes(char);
+        const correctGuess = isGuessed && currentWord.includes(char);
+        const incorrectGuess = isGuessed && !currentWord.includes(char);
 
-        let style = defaultState ? clsx({ key: true }) : clsx({ key: true, right: correctGuess, wrong: incorrectGuess });
+        let style = !isGuessed ? clsx({ key: true }) : clsx({ key: true, right: correctGuess, wrong: incorrectGuess });
 
         return <button onClick={() => guess(char.toLowerCase())} key={char} className={style}>{char}</button>
     });
