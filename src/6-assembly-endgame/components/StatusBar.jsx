749f6ecb8bd1b1c7888d0498b8f8caa1924a2082
pchristou <pierischristou@gmail.com>
2025-06-11 16:29:20 +0100

    6 - assembly: endgame (hangman) - indentation

--- src/6-assembly-endgame/components/StatusBar.jsx
+++ src/6-assembly-endgame/components/StatusBar.jsx
@@ -3,25 +3,25 @@ export function StatusBar({ gameState }) {
     let status = null;
 
     switch (gameState) {
-    case 'incorrect':
-        status = <div className="status-bar status-bar-incorrect">
-            <span>“Farewell HTML & CSS” 🫡</span>
-        </div>;
-        break;
-    case 'won':
-        status = <div className="status-bar status-bar-won">
-            <span className="title">You win!</span>
-            <span>Well done! 🎉</span>
-        </div>;
-        break;
-    case 'lost':
-        status = <div className="status-bar status-bar-lost">
-            <span className="title">Game over!</span>
-            <span>You lose! Better start learning Assembly 😭</span>
-        </div>;
-        break;
-    default:
-        status = null;
+        case 'incorrect':
+            status = <div className="status-bar status-bar-incorrect">
+                <span>“Farewell HTML & CSS” 🫡</span>
+            </div>;
+            break;
+        case 'won':
+            status = <div className="status-bar status-bar-won">
+                <span className="title">You win!</span>
+                <span>Well done! 🎉</span>
+            </div>;
+            break;
+        case 'lost':
+            status = <div className="status-bar status-bar-lost">
+                <span className="title">Game over!</span>
+                <span>You lose! Better start learning Assembly 😭</span>
+            </div>;
+            break;
+        default:
+            status = null;
     }
 
     return (
